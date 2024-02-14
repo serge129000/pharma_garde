b628f279341ef63b4649d7c8c35e989248cae29d
sergeciblApp <serge.gaba@cible-app.com>
2024-02-14 17:29:08 +0000

    adding days repeat

--- services/auto_services.dart
+++ services/auto_services.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_local_variable, avoid_dynamic_calls, inference_failure_on_untyped_parameter
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -11,45 +12,47 @@ import '../models/pharmacie_model.dart';
 
 class AutoServices {
   static Future<void> setAllPharma() async {
-    try {
-      final headers = {
-        'apiKey': 'lE3ivhi3YKtJmsXysghCxzR95nwrPRhc',
-      };
-      final request = http.MultipartRequest('POST',
-          Uri.parse('https://api.kondjigbale.com/pharmacies/all_pharma'));
-      request.fields.addAll({
-        'access_token': 'YqbzM72mVI4hcqX4N1ebJIxmO9LrE',
-        'c_identifiant': 'nUBjx7UCqk9ViHB8tXeVZThizQs6p7dU',
-        'u_identifiant': 'iXZdGtMH3bjh3lRpC5xTyzOApawD9lTi',
-        'device_id': 'HUAWEIVOG-L29',
-        'device_name': 'VOG-L29',
-        'latMember': '6.1743821',
-        'longMember': '1.2272105',
-      });
+    Timer.periodic(const Duration(days: 1), (timer) async {
+      try {
+        final headers = {
+          'apiKey': 'lE3ivhi3YKtJmsXysghCxzR95nwrPRhc',
+        };
+        final request = http.MultipartRequest('POST',
+            Uri.parse('https://api.kondjigbale.com/pharmacies/all_pharma'));
+        request.fields.addAll({
+          'access_token': 'YqbzM72mVI4hcqX4N1ebJIxmO9LrE',
+          'c_identifiant': 'nUBjx7UCqk9ViHB8tXeVZThizQs6p7dU',
+          'u_identifiant': 'iXZdGtMH3bjh3lRpC5xTyzOApawD9lTi',
+          'device_id': 'HUAWEIVOG-L29',
+          'device_name': 'VOG-L29',
+          'latMember': '6.1743821',
+          'longMember': '1.2272105',
+        });
 
-      request.headers.addAll(headers);
+        request.headers.addAll(headers);
 
-      final response = await request.send();
-      if (response.statusCode != 200) {
-        throw Exception("Une erreur s'est produite");
+        final response = await request.send();
+        if (response.statusCode != 200) {
+          throw Exception("Une erreur s'est produite");
+        }
+        final jsonResponse = await response.stream.bytesToString();
+        final data = jsonDecode(jsonResponse);
+        final pharmas = data['information'];
+        final allPharmas = pharmas['pharmas'];
+        final gardes = pharmas['garde'];
+        final gardeFilePath = join(current, 'garde.json');
+        final pharmaFilePath = join(current, 'pharma.json');
+        final gardeFile = await File(gardeFilePath).create();
+        final pharmaFile = await File(pharmaFilePath).create();
+        await gardeFile.writeAsString(jsonEncode(gardes));
+        final allPharmacies =
+            allPharmas.map((e) => PharmacieModel.fromJson(e).toJson()).toList();
+        final garde = GardeModel.fromJson(gardes).toJson();
+        await gardeFile.writeAsString(jsonEncode(garde));
+        await pharmaFile.writeAsString(jsonEncode({'pharmas': allPharmacies}));
+      } catch (e) {
+        rethrow;
       }
-      final jsonResponse = await response.stream.bytesToString();
-      final data = jsonDecode(jsonResponse);
-      final pharmas = data['information'];
-      final allPharmas = pharmas['pharmas'];
-      final gardes = pharmas['garde'];
-      final gardeFilePath = join(current, 'garde.json');
-      final pharmaFilePath = join(current, 'pharma.json');
-      final gardeFile = await File(gardeFilePath).create();
-      final pharmaFile = await File(pharmaFilePath).create();
-      await gardeFile.writeAsString(jsonEncode(gardes));
-      final allPharmacies =
-          allPharmas.map((e) => PharmacieModel.fromJson(e).toJson()).toList();
-      final garde = GardeModel.fromJson(gardes).toJson();
-      await gardeFile.writeAsString(jsonEncode(garde));
-      await pharmaFile.writeAsString(jsonEncode({'pharmas': allPharmacies}));
-    } catch (e) {
-      rethrow;
-    }
+    });
   }
 }
